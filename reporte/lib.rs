7d6a5387aaf34fea7f85cf1446a6249adc557726
Agustin Castillo <agustincasti2002@gmail.com>
2024-07-05 23:33:17 -0300

    contrato reporte terminado

--- reporte/lib.rs
+++ reporte/lib.rs
@@ -8,17 +8,115 @@
 mod reporte {
     use ink::prelude::collections::BTreeMap;
     use ink::prelude::vec::Vec;
-    use sistema_votacion::{Error, SistemaVotacionRef, Usuario};
+    use sistema_votacion::{Error, RolUsuario, SistemaVotacionRef, Usuario};
 
     #[ink(storage)]
     pub struct Reporte {
+        #[cfg(not(test))]
+        //campo que solo va a existir en ambiente de producción y no en ambiente de testing para poder mockearlo en los tests y que no me salga error de mismatch de tipos cuando intento instaciar un reporte
         sistema_votacion: SistemaVotacionRef,
     }
 
     impl Reporte {
+        #[cfg(not(test))]
         #[ink(constructor)]
         pub fn new(sistema_votacion: SistemaVotacionRef) -> Self {
             Self { sistema_votacion }
+        }
+
+        //----------------------------------- Funciones Mock con valores de prueba------------------------------------------------------------
+        #[cfg(test)]
+        pub fn new() -> Self {
+            Self {}
+        }
+
+        #[cfg(test)]
+        fn get_votantes(&self, _id: u64) -> Result<Vec<Usuario>, Error> {
+            let usuario1 = Usuario::new(
+                AccountId::from([0x1; 32]),
+                "Agustin".to_string(),
+                "mail".to_string(),
+                RolUsuario::Votante,
+            );
+            let usuario2 = Usuario::new(
+                AccountId::from([0x2; 32]),
+                "Fer".to_string(),
+                "mail".to_string(),
+                RolUsuario::Votante,
+            );
+            Ok(vec![usuario1, usuario2])
+        }
+
+        #[cfg(test)]
+        fn get_tamanio_elecciones(&self) -> Result<u64, Error> {
+            Ok(2)
+        }
+
+        #[cfg(test)]
+        fn get_fecha_fin(&self, _id: u64) -> Result<u64, Error> {
+            Ok(100000)
+        }
+
+        #[cfg(test)]
+        fn get_fecha_inicio(&self, _id: u64) -> Result<u64, Error> {
+            Ok(500)
+        }
+
+        #[cfg(test)]
+        fn get_votantes_que_votaron(&self, _id: u64) -> Result<Vec<Usuario>, Error> {
+            let usuario1 = Usuario::new(
+                AccountId::from([0x1; 32]),
+                "Agustin".to_string(),
+                "mail".to_string(),
+                RolUsuario::Votante,
+            );
+            let usuario2 = Usuario::new(
+                AccountId::from([0x2; 32]),
+                "Fer".to_string(),
+                "mail".to_string(),
+                RolUsuario::Votante,
+            );
+
+            Ok(vec![usuario1, usuario2])
+        }
+
+        #[cfg(test)]
+        fn get_candidatos(&self, _id: u64) -> Result<BTreeMap<AccountId, u64>, Error> {
+            let mut candidatos = BTreeMap::new();
+            candidatos.insert(AccountId::from([0x1; 32]), 10);
+            candidatos.insert(AccountId::from([0x2; 32]), 5);
+            candidatos.insert(AccountId::from([0x3; 32]), 3);
+            Ok(candidatos)
+        }
+        //------------------------------------------------------------------------------------------------------
+        #[cfg(not(test))]
+        fn get_votantes(&self, id: u64) -> Result<Vec<Usuario>, Error> {
+            self.sistema_votacion.get_votantes(id)
+        }
+
+        #[cfg(not(test))]
+        fn get_tamanio_elecciones(&self) -> Result<u64, Error> {
+            self.sistema_votacion.get_tamanio_elecciones()
+        }
+
+        #[cfg(not(test))]
+        fn get_fecha_fin(&self, id: u64) -> Result<u64, Error> {
+            self.sistema_votacion.get_fecha_fin(id)
+        }
+
+        #[cfg(not(test))]
+        fn get_fecha_inicio(&self, id: u64) -> Result<u64, Error> {
+            self.sistema_votacion.get_fecha_inicio(id)
+        }
+
+        #[cfg(not(test))]
+        fn get_votantes_que_votaron(&self, id: u64) -> Result<Vec<Usuario>, Error> {
+            self.sistema_votacion.get_votantes_que_votaron(id)
+        }
+
+        #[cfg(not(test))]
+        fn get_candidatos(&self, id: u64) -> Result<BTreeMap<AccountId, u64>, Error> {
+            self.sistema_votacion.get_candidatos(id)
         }
 
         fn generar_reporte_registro_votantes_priv(
@@ -26,12 +124,12 @@ mod reporte {
             id: u64,
         ) -> Result<ReporteRegistroVotantes, Error> {
             //verifica que la elección exista
-            if id > self.sistema_votacion.get_tamanio_elecciones()? {
+            if id >= self.get_tamanio_elecciones()? {
                 return Err(Error::EleccionNoExiste);
             }
 
             //traigo los votantes registrados en la elección desde el contrato sistema_votacion
-            let votantes_reg = self.sistema_votacion.get_votantes(id)?;
+            let votantes_reg = self.get_votantes(id)?;
 
             //creo el reporte
             let reporte_registro_votantes = ReporteRegistroVotantes {
@@ -54,24 +152,22 @@ mod reporte {
             &self,
             id: u64,
         ) -> Result<ReporteParticipacion, Error> {
-            let fecha_cierre = self.sistema_votacion.get_fecha_fin(id)?;
-            let fecha_inicio = self.sistema_votacion.get_fecha_inicio(id)?;
+            let fecha_cierre = self.get_fecha_fin(id)?;
+            let fecha_inicio = self.get_fecha_inicio(id)?;
             let fecha_actual = self.env().block_timestamp();
-
-            //verifica que la elección ya haya cerrado
-            if fecha_actual < fecha_cierre {
-                return Err(Error::EleccionAbierta);
-            }
             //verifica que la elección ya haya iniciado
             if fecha_actual < fecha_inicio {
                 return Err(Error::EleccionNoActiva);
             }
+            //verifica que la elección ya haya cerrado
+            if fecha_actual < fecha_cierre {
+                return Err(Error::EleccionAbierta);
+            }
 
             //traigo la cantidad de votos emitidos desde el contrato sistema_votacion y la cantidad de votantes registrados
-            let cantidad_votos_emitidos =
-                self.sistema_votacion.get_votantes_que_votaron(id)?.len() as u64;
+            let cantidad_votos_emitidos = self.get_votantes_que_votaron(id)?.len() as u64;
 
-            let cantidad_votantes = self.sistema_votacion.get_votantes(id)?.len() as u64;
+            let cantidad_votantes = self.get_votantes(id)?.len() as u64;
 
             //calculo el porcentaje de participación
             let mut porcentaje_participacion = cantidad_votos_emitidos
@@ -101,8 +197,8 @@ mod reporte {
         }
 
         fn generar_reporte_resultado_priv(&self, id: u64) -> Result<ReporteResultado, Error> {
-            let fecha_cierre = self.sistema_votacion.get_fecha_fin(id)?;
-            let fecha_inicio = self.sistema_votacion.get_fecha_inicio(id)?;
+            let fecha_cierre = self.get_fecha_fin(id)?;
+            let fecha_inicio = self.get_fecha_inicio(id)?;
             let fecha_actual = self.env().block_timestamp();
 
             //verifica que la elección ya haya cerrado
@@ -115,7 +211,7 @@ mod reporte {
             }
 
             //traigo los resultados de la elección desde el contrato sistema_votacion y los ordeno
-            let resultados_desordenados = self.sistema_votacion.get_candidatos(id)?;
+            let resultados_desordenados = self.get_candidatos(id)?;
             let mut resultados_ordenados = resultados_desordenados.into_iter().collect::<Vec<_>>();
             resultados_ordenados.sort_by(|a, b| b.1.cmp(&a.1));
 
@@ -161,6 +257,81 @@ mod reporte {
 
     #[cfg(test)]
     mod tests {
-        // no supe como simular la data que recibo del contrato sistema_votacion y no pude hacer los test
+
+        use super::*;
+
+        #[ink::test]
+        fn test_generar_reporte_registro_votantes() {
+            let reporte = Reporte::new();
+
+            assert!(reporte.generar_reporte_registro_votantes(1).is_ok());
+            assert!(reporte.generar_reporte_registro_votantes(3).is_err());
+            let reporte_registro_votantes = ReporteRegistroVotantes {
+                nro_eleccion: 1,
+                votantes: vec![
+                    Usuario::new(
+                        AccountId::from([0x1; 32]),
+                        "Agustin".to_string(),
+                        "mail".to_string(),
+                        RolUsuario::Votante,
+                    ),
+                    Usuario::new(
+                        AccountId::from([0x2; 32]),
+                        "Fer".to_string(),
+                        "mail".to_string(),
+                        RolUsuario::Votante,
+                    ),
+                ],
+            };
+            assert_eq!(
+                reporte.generar_reporte_registro_votantes(1).unwrap(),
+                reporte_registro_votantes
+            );
+        }
+        #[ink::test]
+        fn test_generar_reporte_participacion() {
+            let reporte = Reporte::new();
+
+            ink_env::test::set_block_timestamp::<ink_env::DefaultEnvironment>(300);
+            assert!(reporte.generar_reporte_participacion(1).is_err());
+            ink_env::test::set_block_timestamp::<ink_env::DefaultEnvironment>(600);
+            assert!(reporte.generar_reporte_participacion(1).is_err());
+            ink_env::test::set_block_timestamp::<ink_env::DefaultEnvironment>(10000000);
+            assert!(reporte.generar_reporte_participacion(2).is_ok());
+
+            let reporte_participacion = ReporteParticipacion {
+                nro_eleccion: 2,
+                cantidad_votos_emitidos: 2,
+                porcentaje_participacion: 100,
+            };
+            assert_eq!(
+                reporte.generar_reporte_participacion(2).unwrap(),
+                reporte_participacion
+            );
+        }
+        #[ink::test]
+        fn test_generar_reporte_resultado() {
+            let reporte = Reporte::new();
+            ink_env::test::set_block_timestamp::<ink_env::DefaultEnvironment>(300);
+            assert!(reporte.generar_reporte_resultado(1).is_err());
+
+            ink_env::test::set_block_timestamp::<ink_env::DefaultEnvironment>(600);
+            assert!(reporte.generar_reporte_resultado(1).is_err());
+
+            ink_env::test::set_block_timestamp::<ink_env::DefaultEnvironment>(10000000);
+            assert!(reporte.generar_reporte_resultado(2).is_ok());
+            let reporte_resultado = ReporteResultado {
+                nro_eleccion: 2,
+                resultados_ordenados: vec![
+                    (AccountId::from([0x1; 32]), 10),
+                    (AccountId::from([0x2; 32]), 5),
+                    (AccountId::from([0x3; 32]), 3),
+                ],
+            };
+            assert_eq!(
+                reporte.generar_reporte_resultado(2).unwrap(),
+                reporte_resultado
+            );
+        }
     }
 }
